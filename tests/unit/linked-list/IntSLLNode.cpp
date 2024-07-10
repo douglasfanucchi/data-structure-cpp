0af1c1d8b11e493c06534848e0acc455c5f16584
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-10 10:41:45 -0300

    tests: test creates a IntSLLNode

--- tests/unit/linked-list/IntSLLNode.cpp
+++ tests/unit/linked-list/IntSLLNode.cpp
@@ -1,11 +1,15 @@
 #include <asserts.hpp>
+#include <IntSLLNode.hpp>
 
-void test_should_assert_true()
+void test_should_create_a_node()
 {
-    ASSERT_TRUE(true);
+    IntSLLNode node(10);
+
+    ASSERT_EQ(10, node.value);
+    ASSERT_TRUE(0 == node.next);
 }
 
 void RUN_INT_SLL_NODE_SUITE()
 {
-    test_should_assert_true();
+    test_should_create_a_node();
 }
