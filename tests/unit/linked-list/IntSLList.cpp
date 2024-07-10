e5cdce9a21062e156731f18ffb380014e2adb1ef
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-10 11:13:25 -0300

    tests: test creates an empty IntSLList

--- tests/unit/linked-list/IntSLList.cpp
+++ tests/unit/linked-list/IntSLList.cpp
@@ -1,11 +1,15 @@
 #include <asserts.hpp>
+#include <IntSLLNode.hpp>
+#include <IntSLList.hpp>
 
-void test_should_assert_true()
+void test_should_create_an_empty_linked_list()
 {
-    ASSERT_TRUE(true);
+    IntSLList list;
+
+    ASSERT_TRUE(list.isEmpty());
 }
 
 void RUN_INT_SLLIST_SUITE()
 {
-    test_should_assert_true();
+    test_should_create_an_empty_linked_list();
 }
