a73be377ebef8420ecda257d9955a07b43fcd758
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-11 09:11:43 -0300

    tests: test inserts a node into single linked list head

--- tests/unit/linked-list/IntSLList.cpp
+++ tests/unit/linked-list/IntSLList.cpp
@@ -9,7 +9,17 @@ void test_should_create_an_empty_linked_list()
     ASSERT_TRUE(list.isEmpty());
 }
 
+void test_should_insert_node_into_linked_list_head()
+{
+    IntSLList list;
+
+    list.addToHead(5);
+
+    ASSERT_FALSE(list.isEmpty());
+}
+
 void RUN_INT_SLLIST_SUITE()
 {
     test_should_create_an_empty_linked_list();
+    test_should_insert_node_into_linked_list_head();
 }
