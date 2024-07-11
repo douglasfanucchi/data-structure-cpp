8b354351b624791398245905f0813ccd0d18d1f1
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-11 10:14:31 -0300

    tests: test deletes node from start of a linked list

--- tests/unit/linked-list/IntSLList.cpp
+++ tests/unit/linked-list/IntSLList.cpp
@@ -27,9 +27,21 @@ void test_should_insert_node_into_the_end_of_linked_list()
     ASSERT_FALSE(list.isEmpty());
 }
 
+void test_should_delete_node_from_the_start_of_linked_list()
+{
+    IntSLList list;
+    list.addToHead(25);
+
+    int result = list.deleteFromHead();
+
+    ASSERT_EQ(25, result);
+    ASSERT_TRUE(list.isEmpty());
+}
+
 void RUN_INT_SLLIST_SUITE()
 {
     test_should_create_an_empty_linked_list();
     test_should_insert_node_into_linked_list_head();
     test_should_insert_node_into_the_end_of_linked_list();
+    test_should_delete_node_from_the_start_of_linked_list();
 }
