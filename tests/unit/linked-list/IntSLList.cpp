bbb02160b2aa6b157972d680d3a46045a82a2421
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-11 09:34:34 -0300

    tests: test inserts node into the end of single linked list

--- tests/unit/linked-list/IntSLList.cpp
+++ tests/unit/linked-list/IntSLList.cpp
@@ -18,8 +18,18 @@ void test_should_insert_node_into_linked_list_head()
     ASSERT_FALSE(list.isEmpty());
 }
 
+void test_should_insert_node_into_the_end_of_linked_list()
+{
+    IntSLList list;
+
+    list.addToTail(5);
+
+    ASSERT_FALSE(list.isEmpty());
+}
+
 void RUN_INT_SLLIST_SUITE()
 {
     test_should_create_an_empty_linked_list();
     test_should_insert_node_into_linked_list_head();
+    test_should_insert_node_into_the_end_of_linked_list();
 }
