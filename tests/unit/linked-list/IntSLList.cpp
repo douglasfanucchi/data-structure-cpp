9be4572188fd53a45a9b2ac0e172fc049e33426d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-11 14:54:58 -0300

    tests: test deletes node from the end of the linked list that has only one element

--- tests/unit/linked-list/IntSLList.cpp
+++ tests/unit/linked-list/IntSLList.cpp
@@ -38,10 +38,22 @@ void test_should_delete_node_from_the_start_of_linked_list()
     ASSERT_TRUE(list.isEmpty());
 }
 
+void test_should_delete_node_from_the_end_of_linked_list_when_list_has_one_element()
+{
+    IntSLList list;
+    list.addToTail(25);
+
+    int result = list.deleteFromTail();
+
+    ASSERT_EQ(25, result);
+    ASSERT_TRUE(list.isEmpty());
+}
+
 void RUN_INT_SLLIST_SUITE()
 {
     test_should_create_an_empty_linked_list();
     test_should_insert_node_into_linked_list_head();
     test_should_insert_node_into_the_end_of_linked_list();
     test_should_delete_node_from_the_start_of_linked_list();
+    test_should_delete_node_from_the_end_of_linked_list_when_list_has_one_element();
 }
