72f50987868fd44a3e054ee027a38bf7211828d0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-12 13:06:11 -0300

    tests: test creates DLLNode with multiple types

--- tests/unit/linked-list/doubly-linked-list/DLLNode.cpp
+++ tests/unit/linked-list/doubly-linked-list/DLLNode.cpp
@@ -1,6 +1,26 @@
 #include <asserts.hpp>
+#include <DLLNode.hpp>
 
+void test_should_create_dll_node()
+{
+    DLLNode<int> intNode(1);
+    ASSERT_TRUE(intNode.next == NULL);
+    ASSERT_TRUE(intNode.prev == NULL);
+
+    DLLNode<char> charNode('1');
+    ASSERT_TRUE(charNode.next == NULL);
+    ASSERT_TRUE(charNode.prev == NULL);
+
+    DLLNode<std::string> stringNode("ola mundo");
+    ASSERT_TRUE(stringNode.next == NULL);
+    ASSERT_TRUE(stringNode.prev == NULL);
+
+    DLLNode<float> floatNode(1.0);
+    ASSERT_TRUE(floatNode.next == NULL);
+    ASSERT_TRUE(floatNode.prev == NULL);
+}
 
 void RUN_DLL_NODE_SUITE()
 {
+    test_should_create_dll_node();
 }
