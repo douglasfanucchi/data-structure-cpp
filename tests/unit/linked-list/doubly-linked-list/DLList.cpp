5138fc68265fdd12b84700374f5981c0e8764592
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-12 14:17:41 -0300

    tests: test inserts node into the head of a one-node double linked list

--- tests/unit/linked-list/doubly-linked-list/DLList.cpp
+++ tests/unit/linked-list/doubly-linked-list/DLList.cpp
@@ -8,7 +8,17 @@ void test_should_create_double_linked_list()
     ASSERT_TRUE(list.isEmpty());
 }
 
+void test_should_insert_node_into_the_start_of_one_node_double_linked_list()
+{
+    DLList<std::string> list;
+    
+    list.addToHead("hello");
+
+    ASSERT_FALSE(list.isEmpty());
+}
+
 void RUN_DOUBLE_LINKED_LIST_SUITE()
 {
     test_should_create_double_linked_list();
+    test_should_insert_node_into_the_start_of_one_node_double_linked_list();
 }
