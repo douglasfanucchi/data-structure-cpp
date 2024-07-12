8f884a87b1daddf89a3edcb83ef0deb4bd63e97e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-12 13:06:54 -0300

    feat: implements DLLNode constructor using templates

---
+++ includes/DLLNode.hpp
@@ -0,0 +1,22 @@
+#ifndef DLL_NODE_H
+# define DLL_NODE_H
+
+#include <cstddef>
+
+template<typename T>
+class DLLNode {
+    public:
+        DLLNode(const T& value, DLLNode *prev = NULL, DLLNode *next = NULL);
+        T value;
+        DLLNode *next;
+        DLLNode *prev;
+};
+
+#endif
+
+template<typename T>
+DLLNode<T>::DLLNode(const T &value, DLLNode *prev, DLLNode *next) {
+    this->value = value;
+    this->next = next;
+    this->prev = prev;
+}
