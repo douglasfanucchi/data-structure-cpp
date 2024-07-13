6e851274536dd24d358f6b3cdd8beb2ce1f84df8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-13 08:55:44 -0300

    tests: test delets a node from the end of a multiple-node doubly linked list

--- tests/unit/linked-list/doubly-linked-list/DLList.cpp
+++ tests/unit/linked-list/doubly-linked-list/DLList.cpp
@@ -50,6 +50,20 @@ void test_should_delete_a_node_from_a_one_node_doubly_linked_list()
     ASSERT_TRUE(list.isEmpty());
 }
 
+void test_should_delete_a_node_from_the_end_of_a_multiple_node_doubly_linked_list()
+{
+    DLList<std::string> list;
+    const char *expected = "world";
+    list.addToTail("hello");
+    list.addToTail("world");
+
+    std::string result = list.deleteFromTail();
+    list.deleteFromTail();
+
+    ASSERT_STREQ(expected, result);
+    ASSERT_TRUE(list.isEmpty());
+}
+
 void RUN_DOUBLE_LINKED_LIST_SUITE()
 {
     test_should_create_double_linked_list();
@@ -57,4 +71,5 @@ void RUN_DOUBLE_LINKED_LIST_SUITE()
     test_should_insert_node_into_the_end_of_an_empty_double_linked_list();
     test_should_try_to_delete_a_node_from_an_empty_double_linked_list();
     test_should_delete_a_node_from_a_one_node_doubly_linked_list();
+    test_should_delete_a_node_from_the_end_of_a_multiple_node_doubly_linked_list();
 }
