6d77dba13f514fd56f49c6ca2c1e141103d17753
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-13 15:56:48 -0300

    feat: implements CDLList<T>::deleteFromTail(void)

--- includes/CDLList.hpp
+++ includes/CDLList.hpp
@@ -13,6 +13,7 @@ class CDLList {
         bool isEmpty(void);
         void addToTail(const T&);
         void addToHead(const T&);
+        T deleteFromTail(void);
         ~CDLList(void);
 };
 
@@ -57,6 +58,25 @@ void CDLList<T>::addToHead(const T &value) {
         return this->addToTail(value);
     }
     this->tail->next = new DLLNode<T>(value, this->tail, this->tail->next);
+}
+
+template<typename T>
+T CDLList<T>::deleteFromTail(void) {
+    if (this->isEmpty()) {
+        throw ("empty list");
+    }
+    T value = this->tail->value;
+    if (this->tail->next == this->tail) {
+        delete this->tail;
+        this->tail = 0;
+        return value;
+    }
+    DLLNode<T> *tmp = this->tail;
+    this->tail->prev->next = this->tail->next;
+    this->tail->next->prev = this->tail->prev;
+    this->tail = this->tail->prev;
+    delete tmp;
+    return value;
 }
 
 #endif
