f1d670515eb696332dc01b91ddcae88becce6880
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-13 14:15:25 -0300

    tests: test inserts node into the end of circular doubly linked list

--- tests/unit/linked-list/doubly-linked-list/CDLList.cpp
+++ tests/unit/linked-list/doubly-linked-list/CDLList.cpp
@@ -8,7 +8,17 @@ void test_should_create_circular_doubly_linked_list()
     ASSERT_TRUE(list.isEmpty());
 }
 
+void test_should_insert_node_into_the_end_of_a_circular_doubly_linked_list()
+{
+    CDLList<int> list;
+
+    list.addToTail(10);
+
+    ASSERT_FALSE(list.isEmpty());
+}
+
 void RUN_CIRCULAR_DOUBLY_LINKED_LIST_SUITE()
 {
     test_should_create_circular_doubly_linked_list();
+    test_should_insert_node_into_the_end_of_a_circular_doubly_linked_list();
 }
