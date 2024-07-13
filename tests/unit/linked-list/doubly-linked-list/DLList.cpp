2be1c7605e245d865cf6cba8f07078d723382e68
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-13 10:58:39 -0300

    tests: test searches for an element in an empty doubly linked list

--- tests/unit/linked-list/doubly-linked-list/DLList.cpp
+++ tests/unit/linked-list/doubly-linked-list/DLList.cpp
@@ -178,6 +178,15 @@ void test_should_search_for_a_node_in_the_doubly_linked_list()
     ASSERT_TRUE(result);
 }
 
+void test_should_search_for_a_node_in_an_empty_doubly_linked_list()
+{
+    DLList<std::string> list;
+
+    bool result = list.isInList("hello");
+
+    ASSERT_FALSE(result);
+}
+
 void RUN_DOUBLY_LINKED_LIST_SUITE()
 {
     test_should_create_doubly_linked_list();
@@ -195,4 +204,5 @@ void RUN_DOUBLY_LINKED_LIST_SUITE()
     test_should_delete_a_node_from_the_start_of_a_one_node_doubly_linked_list_using_deleteNode();
     test_should_delete_a_node_from_the_end_of_a_multiple_node_doubly_linked_list_using_deleteNode();
     test_should_search_for_a_node_in_the_doubly_linked_list();
+    test_should_search_for_a_node_in_an_empty_doubly_linked_list();
 }
