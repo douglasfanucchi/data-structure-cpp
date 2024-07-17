ac8ec270e87e859e1550312b6fc870633e5ec43a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-17 16:01:27 -0300

    tests: test creates an empty Stack

---
+++ includes/Stack.hpp
@@ -0,0 +1,9 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <DLList.hpp>
+
+template<typename T>
+class Stack : public DLList<T> {};
+
+#endif
--- tests/unit/stack/Stack.cpp
+++ tests/unit/stack/Stack.cpp
@@ -1,11 +1,14 @@
 #include <asserts.hpp>
+#include <Stack.hpp>
 
-void test_should_assert_true()
+void test_should_create_empty_stack()
 {
-    ASSERT_TRUE(true);
+    Stack<int> stack;
+
+    ASSERT_TRUE(stack.isEmpty());
 }
 
 void RUN_STACK_TEST_SUITE()
 {
-    test_should_assert_true();
+    test_should_create_empty_stack();
 }
