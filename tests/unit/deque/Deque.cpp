8853ede9e7dfc58415ea91d27cd1f1811dc8c592
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-20 17:22:21 -0300

    tests: test inserts eight elements into deque

--- tests/unit/deque/Deque.cpp
+++ tests/unit/deque/Deque.cpp
@@ -27,9 +27,26 @@ void test_should_insert_two_elements_into_deque_front()
     ASSERT_FALSE(deque.isEmpty());
 }
 
+void test_should_insert_eight_elements_into_deque()
+{
+    Deque<int> deque;
+
+    deque.pushFront(1);
+    deque.pushFront(2);
+    deque.pushFront(3);
+    deque.pushFront(4);
+    deque.pushFront(5);
+    deque.pushFront(6);
+    deque.pushFront(7);
+    deque.pushFront(8);
+
+    ASSERT_FALSE(deque.isEmpty());
+}
+
 void RUN_DEQUE_TEST_SUITE()
 {
     test_should_create_a_deque();
     test_should_insert_element_into_deque();
     test_should_insert_two_elements_into_deque_front();
+    test_should_insert_eight_elements_into_deque();
 }
