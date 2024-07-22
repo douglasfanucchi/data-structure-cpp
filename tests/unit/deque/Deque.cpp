d33ffa9951abe23426d3e38ae7e59dbdccb41181
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-21 22:12:41 -0300

    tests: test inserts multiple elements into deque's end and fornt interchangeably and pop them all

--- tests/unit/deque/Deque.cpp
+++ tests/unit/deque/Deque.cpp
@@ -217,6 +217,55 @@ void test_should_insert_multiple_elements_into_deques_end_and_pop_them_all()
     ASSERT_EQ(11, deque.popFront());
 }
 
+void test_should_insert_multiple_elements_into_deques_end_and_front_interchangeably_and_pop_them_all()
+{
+    Deque<int> deque;
+
+    deque.pushFront(1);
+    deque.pushBack(2);
+    deque.pushFront(3);
+    deque.pushBack(4);
+    deque.pushFront(5);
+    deque.pushBack(6);
+    deque.pushFront(7);
+    deque.pushBack(8);
+    deque.pushFront(9);
+    deque.pushBack(10);
+    deque.pushFront(11);
+    deque.pushBack(12);
+    deque.pushFront(13);
+    deque.pushBack(14);
+    deque.pushFront(15);
+    deque.pushBack(16);
+    deque.pushFront(17);
+    deque.pushBack(18);
+    deque.pushFront(19);
+    deque.pushBack(20);
+    deque.pushFront(21);
+
+    ASSERT_EQ(21, deque.popFront());
+    ASSERT_EQ(19, deque.popFront());
+    ASSERT_EQ(17, deque.popFront());
+    ASSERT_EQ(15, deque.popFront());
+    ASSERT_EQ(13, deque.popFront());
+    ASSERT_EQ(11, deque.popFront());
+    ASSERT_EQ(9, deque.popFront());
+    ASSERT_EQ(7, deque.popFront());
+    ASSERT_EQ(5, deque.popFront());
+    ASSERT_EQ(3, deque.popFront());
+    ASSERT_EQ(1, deque.popFront());
+    ASSERT_EQ(2, deque.popFront());
+    ASSERT_EQ(4, deque.popFront());
+    ASSERT_EQ(6, deque.popFront());
+    ASSERT_EQ(8, deque.popFront());
+    ASSERT_EQ(12, deque.popFront());
+    ASSERT_EQ(14, deque.popFront());
+    ASSERT_EQ(16, deque.popFront());
+    ASSERT_EQ(10, deque.popFront());
+    ASSERT_EQ(18, deque.popFront());
+    ASSERT_EQ(20, deque.popFront());
+}
+
 void RUN_DEQUE_TEST_SUITE()
 {
     test_should_create_a_deque();
@@ -235,4 +284,5 @@ void RUN_DEQUE_TEST_SUITE()
     test_should_insert_multiple_elements_into_deques_end();
     test_should_insert_multiple_elements_into_deques_end_and_two_into_its_front();
     test_should_insert_multiple_elements_into_deques_end_and_pop_them_all();
+    test_should_insert_multiple_elements_into_deques_end_and_front_interchangeably_and_pop_them_all();
 }
