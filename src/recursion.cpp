ac00ecaf551d8b90aa332279cbc9d42f541cae1e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-23 12:48:49 -0300

    feat: implements noRecursiveReverseWord(const std::string &)

--- src/recursion.cpp
+++ src/recursion.cpp
@@ -6,3 +6,15 @@ std::string recursiveReverseWord(const std::string &str) {
     }
     return recursiveReverseWord(str.substr(1)) + str.at(0);
 }
+
+std::string noRecursiveReverseWord(const std::string &str) {
+    size_t top = str.length();
+    char stack[top];
+    std::string result("");
+
+    for(int i = 0; i < top; i++)
+        stack[i] = str.at(i);
+    for(;top > 0;)
+        result += stack[--top];
+    return result;
+}
