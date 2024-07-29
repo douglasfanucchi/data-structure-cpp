35a4d1283786fc1cc095cc1f33e1672c40d7798f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-29 12:44:03 -0300

    tests: test inserts an element into the tree

--- tests/unit/tree/binary-tree/BST.cpp
+++ tests/unit/tree/binary-tree/BST.cpp
@@ -8,7 +8,17 @@ void test_should_create_a_bst(void)
     ASSERT_TRUE(tree.isEmpty());
 }
 
+void test_should_insert_node_into_bst(void)
+{
+    BST<int> tree;
+
+    tree.insert(13);
+
+    ASSERT_FALSE(tree.isEmpty());
+}
+
 void RUN_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_a_bst();
+    test_should_insert_node_into_bst();
 }
