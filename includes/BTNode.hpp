84b7aed50b4dee91ef2894d9d1face5b5d5a8c30
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-29 12:23:40 -0300

    feat: implements BTNode<T>::BTNode(const T&, BTNode<T>*, BTNode<T>*)

--- includes/BTNode.hpp
+++ includes/BTNode.hpp
@@ -6,7 +6,11 @@ class BTNode {
     public:
         T value;
         BTNode(void) : left(0), right(0) {}
+        BTNode(const T &value, BTNode<T> *left = 0, BTNode<T> *right = 0);
         BTNode<T> *left, *right;
 };
+
+template<typename T>
+BTNode<T>::BTNode(const T &value, BTNode<T> *left, BTNode<T> *right) : value(value), left(left), right(right) {}
 
 #endif
