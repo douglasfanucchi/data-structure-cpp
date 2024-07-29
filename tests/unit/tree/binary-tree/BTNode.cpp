f35247dc05e9025be5e5bb011a380fffe07255bf
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-29 12:19:58 -0300

    tests: creates binary tree node with value

--- tests/unit/tree/binary-tree/BTNode.cpp
+++ tests/unit/tree/binary-tree/BTNode.cpp
@@ -9,7 +9,17 @@ void test_should_create_binary_tree_node_with_no_value(void)
     ASSERT_TRUE(node.right == 0);
 }
 
+void test_should_create_binary_tree_node_with_value()
+{
+    BTNode<int> node(1);
+    
+    ASSERT_TRUE(node.value == 1);
+    ASSERT_TRUE(node.left == 0);
+    ASSERT_TRUE(node.right == 0);
+}
+
 void RUN_BINARY_TREE_NODE_TEST_SUITE()
 {
     test_should_create_binary_tree_node_with_no_value();
+    test_should_create_binary_tree_node_with_value();
 }
