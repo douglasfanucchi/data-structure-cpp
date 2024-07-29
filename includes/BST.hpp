0e0ec4a144062a0c5f69f19895c45e14994c9cf3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-29 12:29:25 -0300

    feat: implements BST<T>::BST(void) and BST<T>::isEmpty(void)"

--- includes/BST.hpp
+++ includes/BST.hpp
@@ -1,10 +1,21 @@
 #ifndef BST_H
 # define BST_H
 
+#include <BTNode.hpp>
+
 template<typename T>
 class BST {
+    protected:
+        BTNode<T> *root;
+
     public:
+        BST(void) : root(0) {}
         bool isEmpty(void);
 };
+
+template<typename T>
+bool BST<T>::isEmpty(void) {
+    return this->root == 0;
+}
 
 #endif
