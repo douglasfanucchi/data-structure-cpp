e4cf99afb49ed4edd345d07a7862a87b63c5a27e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-29 12:48:35 -0300

    tests: test inserts node into left subtree

--- tests/unit/tree/binary-tree/BST.cpp
+++ tests/unit/tree/binary-tree/BST.cpp
@@ -17,8 +17,19 @@ void test_should_insert_node_into_bst(void)
     ASSERT_FALSE(tree.isEmpty());
 }
 
+void test_should_insert_node_into_left_subtree(void)
+{
+    BST<int> tree;
+    tree.insert(13);
+
+    tree.insert(10);
+
+    ASSERT_FALSE(tree.isEmpty());
+}
+
 void RUN_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_a_bst();
     test_should_insert_node_into_bst();
+    test_should_insert_node_into_left_subtree();
 }
