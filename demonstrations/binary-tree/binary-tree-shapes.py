0add69398a94348ec0ef389671b58eb2fc488446
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-03 07:10:17 -0300

    implements generation of a complete BST image

--- demonstrations/binary-tree/binary-tree-shapes.py
+++ demonstrations/binary-tree/binary-tree-shapes.py
@@ -21,3 +21,23 @@ class WorstBinaryTreeShape(Scene):
                     )
         graph.scale(1.5)
         self.add(graph)
+
+class BestBinaryTreeShape(Scene):
+  def construct(self):
+    vertices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    edges = [(1, 2), (2, 3), (2, 4), (3, 5), (3, 6), (4, 7), (4, 8), (1, 9), (9, 10), (9, 11), (10, 12), (10, 13), (11, 14), (11, 15)]
+    graph = Graph(
+      vertices,
+      layout='tree',
+      edges=edges,
+      root_vertex=1,
+      vertex_config={
+        'radius': 0.3,
+        'fill_color': ManimColor("#808080")
+      },
+      edge_config={
+        'stroke_color': ManimColor("#808080")
+      },
+      layout_scale=6.5,
+    )
+    self.add(graph)
