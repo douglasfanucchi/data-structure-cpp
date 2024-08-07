c8b91bccf116d4de0f08fbccdb089d812dd5e666
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-07 16:55:37 -0300

    tests: test traverse BST in a deepth-first, preorder way

--- tests/unit/tree/binary-tree/BST.cpp
+++ tests/unit/tree/binary-tree/BST.cpp
@@ -182,6 +182,32 @@ void test_should_traverse_an_empty_bst_in_a_deepthFirst_inorder_way()
     ASSERT_TRUE(result.empty());
 }
 
+void test_should_traverse_bst_in_a_deepthFirst_preorder_way()
+{
+    BST<int> tree;
+    std::queue<int> expected;
+    tree.insert(13);
+    tree.insert(10);
+    tree.insert(2);
+    tree.insert(12);
+    tree.insert(25);
+    tree.insert(20);
+    tree.insert(31);
+    tree.insert(29);
+    expected.push(13);
+    expected.push(10);
+    expected.push(2);
+    expected.push(12);
+    expected.push(25);
+    expected.push(20);
+    expected.push(31);
+    expected.push(29);
+
+    std::queue<int> result = tree.preorder();
+
+    ASSERT_TRUE(expected == result);
+}
+
 void RUN_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_a_bst();
@@ -197,4 +223,5 @@ void RUN_BINARY_SEARCH_TREE_TEST_SUITE()
     test_should_traverse_empty_bst_in_a_breadthFirst_way();
     test_should_traverse_bst_in_a_deepthFirst_way_inorder();
     test_should_traverse_an_empty_bst_in_a_deepthFirst_inorder_way();
+    test_should_traverse_bst_in_a_deepthFirst_preorder_way();
 }
