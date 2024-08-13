16d116382ee33223365b10daf6505f3be7cb1f94
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-13 11:40:41 -0300

    tests: test inserts element into TBST

--- tests/unit/tree/binary-tree/TBST.cpp
+++ tests/unit/tree/binary-tree/TBST.cpp
@@ -8,7 +8,17 @@ void test_should_create_an_empty_tbst(void)
     ASSERT_TRUE(tree.isEmpty());
 }
 
+void test_should_insert_element_into_tbst(void)
+{
+    TBST<int> tree;
+
+    tree.insert(1);
+
+    ASSERT_FALSE(tree.isEmpty());
+}
+
 void RUN_THREADED_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_an_empty_tbst();
+    test_should_insert_element_into_tbst();
 }
