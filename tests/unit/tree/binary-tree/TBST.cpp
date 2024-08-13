3c8ce50f4ccf2fdcad0f2d4b05b8f475942829d6
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-13 11:43:59 -0300

    tests: test traverses TBST inorder deepth-first

--- tests/unit/tree/binary-tree/TBST.cpp
+++ tests/unit/tree/binary-tree/TBST.cpp
@@ -1,5 +1,6 @@
 #include <asserts.hpp>
 #include <TBST.hpp>
+#include <queue>
 
 void test_should_create_an_empty_tbst(void)
 {
@@ -17,8 +18,25 @@ void test_should_insert_element_into_tbst(void)
     ASSERT_FALSE(tree.isEmpty());
 }
 
+void test_should_traverse_tbst_deepthFirst_inorder(void)
+{
+    TBST<int> tree;
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(1);
+    std::queue<int> expected;
+    expected.push(1);
+    expected.push(2);
+    expected.push(3);
+
+    std::queue<int> result = tree.inorder();
+
+    ASSERT_TRUE(expected == result);
+}
+
 void RUN_THREADED_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_an_empty_tbst();
     test_should_insert_element_into_tbst();
+    test_should_traverse_tbst_deepthFirst_inorder();
 }
