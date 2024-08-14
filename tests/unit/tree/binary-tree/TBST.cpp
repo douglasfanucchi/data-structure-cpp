b37e3ceee6e70bec1d7499078257a7e3db86d3b0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-14 11:26:20 -0300

    tests: adds test case to TBST inorder traversal

--- tests/unit/tree/binary-tree/TBST.cpp
+++ tests/unit/tree/binary-tree/TBST.cpp
@@ -32,6 +32,23 @@ void test_should_traverse_tbst_deepthFirst_inorder(void)
     std::queue<int> result = tree.inorder();
 
     ASSERT_TRUE(expected == result);
+
+    {
+        TBST<int> tree;
+        tree.insert(1);
+        tree.insert(0);
+        tree.insert(3);
+        tree.insert(2);
+        std::queue<int> expected;
+        expected.push(0);
+        expected.push(1);
+        expected.push(2);
+        expected.push(3);
+
+        result = tree.inorder();
+
+        ASSERT_TRUE(expected == result);
+    }
 }
 
 void RUN_THREADED_BINARY_SEARCH_TREE_TEST_SUITE()
