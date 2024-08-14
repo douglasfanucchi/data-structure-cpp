2a4b674eee4059bd5ead933cef0fa58685bd4a7f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-08-14 14:24:56 -0300

    tests: test traverses preorder a threaded binary search linked-list-like tree

--- tests/unit/tree/binary-tree/TBST.cpp
+++ tests/unit/tree/binary-tree/TBST.cpp
@@ -51,9 +51,28 @@ void test_should_traverse_tbst_deepthFirst_inorder(void)
     }
 }
 
+void test_should_traverse_tbst_preorder(void)
+{
+    {
+        TBST<int> tree;
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(1);
+        std::queue<int> expected;
+        expected.push(3);
+        expected.push(2);
+        expected.push(1);
+
+        std::queue<int> result = tree.preorder();
+
+        ASSERT_TRUE(expected == result);
+    }
+}
+
 void RUN_THREADED_BINARY_SEARCH_TREE_TEST_SUITE()
 {
     test_should_create_an_empty_tbst();
     test_should_insert_element_into_tbst();
     test_should_traverse_tbst_deepthFirst_inorder();
+    test_should_traverse_tbst_preorder();
 }
