ee5ba31e88ca57797ccdfd82b5ccd4aaff567dc3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-21 09:23:51 -0300

    feat: implements DGraphMatrix::insertEdge(int, int)

--- includes/DGraphMatrix.hpp
+++ includes/DGraphMatrix.hpp
@@ -6,6 +6,7 @@
 class DGraphMatrix : public GraphMatrix {
     public:
         DGraphMatrix(int);
+        void insertEdge(int, int);
 };
 
 #endif
--- src/graphs/DGraphMatrix.cpp
+++ src/graphs/DGraphMatrix.cpp
@@ -1,3 +1,12 @@
 #include <DGraphMatrix.hpp>
+#include <iostream>
 
 DGraphMatrix::DGraphMatrix(int n) : GraphMatrix(n) {}
+
+void DGraphMatrix::insertEdge(int v0, int v1) {
+    if (!this->isValidVertex(v0) || !this->isValidVertex(v1)) {
+        throw "invalid vertex";
+    }
+    this->_matrix[v0][v1] = true;
+    this->_edges++;
+}
