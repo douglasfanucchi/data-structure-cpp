585a82ef8c7d1b3bbadafa5707f6371ceba78245
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-21 11:13:14 -0300

    tests: checks for vertex degree of a DGraph

--- tests/unit/graphs/DGraphMatrix.cpp
+++ tests/unit/graphs/DGraphMatrix.cpp
@@ -79,6 +79,22 @@ void test_should_delete_edge_passing_invalid_vertices()
     delete graph;
 }
 
+void test_should_check_for_vertex_degree()
+{
+    GraphMatrix *graph = new DGraphMatrix(5);
+    graph->insertEdge(1, 0);
+    graph->insertEdge(0, 1);
+    graph->insertEdge(2, 1);
+    graph->insertEdge(4, 1);
+
+    ASSERT_EQ(4, graph->degree(1));
+
+    graph->insertEdge(1, 1);
+    ASSERT_EQ(6, graph->degree(1));
+
+    delete graph;
+}
+
 void RUN_DGRAPH_MATRIX_TEST_SUITE()
 {
     test_should_created_a_dgraph_with_no_edges();
@@ -86,4 +102,5 @@ void RUN_DGRAPH_MATRIX_TEST_SUITE()
     test_should_insert_edge_with_invalid_vertex_into_dgraph();
     test_should_delete_edge_from_dgraph();
     test_should_delete_edge_passing_invalid_vertices();
+    test_should_check_for_vertex_degree();
 }
