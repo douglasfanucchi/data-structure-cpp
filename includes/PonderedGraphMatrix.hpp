2f8ecc418232123b256b07aa2a37c90dad03e350
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-22 16:08:09 -0300

    feat: implements PonderedGraphMatrix constructor and destructor, PonderedGraphMatrix::countEdges(void) and PonderedGraphMatrix::insertEdge(int, int, float)

--- includes/PonderedGraphMatrix.hpp
+++ includes/PonderedGraphMatrix.hpp
@@ -1,6 +1,19 @@
 #ifndef PONDERED_GRAPH_MATRIX_H
 # define PONDERED_GRAPH_MATRIX_H
 
-class PonderedGraphMatrix {};
+#define DEFAULT_VALUE 3.40282347e+38F
+
+class PonderedGraphMatrix {
+    public:
+        PonderedGraphMatrix(int);
+        ~PonderedGraphMatrix(void);
+        int countEdges(void) const;
+        void insertEdge(int, int, float);
+    protected:
+        int _n;
+        int _edges;
+        float **_matrix;
+        bool isValidVertex(int) const;
+};
 
 #endif
--- src/graphs/PonderedGraphMatrix.cpp
+++ src/graphs/PonderedGraphMatrix.cpp
@@ -1,1 +1,37 @@
 #include <PonderedGraphMatrix.hpp>
+
+PonderedGraphMatrix::PonderedGraphMatrix(int n) : _n(n), _edges(0) {
+    this->_matrix = new float*[n];
+    for (int i = 0; i < n; i++) {
+        this->_matrix[i] = new float[n];
+        for (int j = 0; j < n; j++) {
+            this->_matrix[i][j] = DEFAULT_VALUE;
+        }
+    }
+}
+
+int PonderedGraphMatrix::countEdges(void) const {
+    return this->_edges;
+}
+
+void PonderedGraphMatrix::insertEdge(int v0, int v1, float value) {
+    if (!this->isValidVertex(v0) || !this->isValidVertex(v1)) {
+        throw "invalid vertex";
+    }
+    if (DEFAULT_VALUE - this->_matrix[v0][v1] < 0.0001) {
+        this->_edges++;
+    }
+    this->_matrix[v0][v1] = value;
+    this->_matrix[v1][v0] = value;
+}
+
+bool PonderedGraphMatrix::isValidVertex(int v) const {
+    return v > -1 && v < this->_n;
+}
+
+PonderedGraphMatrix::~PonderedGraphMatrix(void) {
+    for (int i = 0; i < this->_n; i++) {
+        delete this->_matrix[i];
+    }
+    delete this->_matrix;
+}
