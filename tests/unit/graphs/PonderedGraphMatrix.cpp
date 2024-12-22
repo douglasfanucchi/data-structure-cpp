9a982f8e889bef4090382dbb8e2b0b7ef1c83abd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-22 15:17:46 -0300

    tests: inserts an edge into pondered graph matrix

--- tests/unit/graphs/PonderedGraphMatrix.cpp
+++ tests/unit/graphs/PonderedGraphMatrix.cpp
@@ -8,7 +8,17 @@ void test_should_create_a_pondered_graph_matrix()
     ASSERT_EQ(0, graph.countEdges());
 }
 
+void test_should_insert_an_edge_into_pondered_graph_matrix()
+{
+    PonderedGraphMatrix graph(5);
+
+    graph.insertEdge(0, 1, 0.5);
+
+    ASSERT_EQ(1, graph.countEdges());
+}
+
 void RUN_PONDERED_GRAPH_MATRIX_TEST_SUITE()
 {
     test_should_create_a_pondered_graph_matrix();
+    test_should_insert_an_edge_into_pondered_graph_matrix();
 }
