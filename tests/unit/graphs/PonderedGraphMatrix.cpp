0b7c2cbc975b1d79f4114769552a0ffab9e4998a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-22 16:02:53 -0300

    tests: inserts invalid edge into pondered graph matrix

--- tests/unit/graphs/PonderedGraphMatrix.cpp
+++ tests/unit/graphs/PonderedGraphMatrix.cpp
@@ -28,9 +28,37 @@ void test_should_insert_update_edge_value()
     ASSERT_EQ(1, graph.countEdges());
 }
 
+void test_should_insert_invalid_edge()
+{
+    PonderedGraphMatrix graph(5);
+    char const *expected = "invalid vertex";
+
+    try {
+        graph.insertEdge(-1, 4, 0.5);
+        ASSERT_TRUE(false);
+    } catch(char const *err) {
+        ASSERT_STREQ(expected, err);
+    }
+
+    try {
+        graph.insertEdge(4, -1, 0.5);
+        ASSERT_TRUE(false);
+    } catch(char const *err) {
+        ASSERT_STREQ(expected, err);
+    }
+
+    try {
+        graph.insertEdge(5, 0, 0.5);
+        ASSERT_TRUE(false);
+    } catch(char const *err) {
+        ASSERT_STREQ(expected, err);
+    }
+}
+
 void RUN_PONDERED_GRAPH_MATRIX_TEST_SUITE()
 {
     test_should_create_a_pondered_graph_matrix();
     test_should_insert_an_edge_into_pondered_graph_matrix();
     test_should_insert_update_edge_value();
+    test_should_insert_invalid_edge();
 }
