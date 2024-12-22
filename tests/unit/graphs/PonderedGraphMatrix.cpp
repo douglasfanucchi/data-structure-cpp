5e87ce911db210575247aa93d69de0a565dc13b9
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-22 15:09:03 -0300

    tests: creates an empty pondered graph

--- tests/unit/graphs/PonderedGraphMatrix.cpp
+++ tests/unit/graphs/PonderedGraphMatrix.cpp
@@ -1,5 +1,14 @@
 #include <asserts.hpp>
 #include <PonderedGraphMatrix.hpp>
 
+void test_should_create_a_pondered_graph_matrix()
+{
+    PonderedGraphMatrix graph(5);
+
+    ASSERT_EQ(0, graph.countEdges());
+}
+
 void RUN_PONDERED_GRAPH_MATRIX_TEST_SUITE()
-{}
+{
+    test_should_create_a_pondered_graph_matrix();
+}
