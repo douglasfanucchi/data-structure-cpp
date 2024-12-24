351db0ee7b1e566ab902d016934abcecf6392428
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-24 07:48:09 -0300

    tests: inserts edges into graph

--- tests/unit/graphs/GraphLinkedList.cpp
+++ tests/unit/graphs/GraphLinkedList.cpp
@@ -8,7 +8,18 @@ void test_should_create_graph()
     ASSERT_EQ(0, graph.countEdges());
 }
 
+void test_should_insert_edge_into_graph()
+{
+    GraphLinkedList graph(5);
+
+    graph.insertEdge(0, 1);
+    graph.insertEdge(1, 0);
+
+    ASSERT_EQ(1, graph.countEdges());
+}
+
 void RUN_GRAPH_LINKED_LIST_TEST_SUITE()
 {
     test_should_create_graph();
+    test_should_insert_edge_into_graph();
 }
