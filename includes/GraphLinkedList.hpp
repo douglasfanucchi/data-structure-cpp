9cf15e933d9b8f43a914017a0ed16a3653149183
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-24 10:37:26 -0300

    feat: implements GraphLinkedList

--- includes/GraphLinkedList.hpp
+++ includes/GraphLinkedList.hpp
@@ -1,6 +1,22 @@
 #ifndef GRAPH_LINKED_LIST_H
 # define GRAPH_LINKED_LIST_H
 
-class GraphLinkedList {};
+#include <IntSLList.hpp>
+
+class GraphLinkedList {
+    public:
+        GraphLinkedList(int);
+        ~GraphLinkedList(void);
+        int countEdges(void) const;
+        void insertEdge(int, int);
+        bool edgeExists(int, int) const;
+        void deleteEdge(int, int);
+
+    protected:
+        int _edges;
+        int _n;
+        IntSLList **_adjacents;
+        bool isValidVertex(int) const;
+};
 
 #endif
--- src/graphs/GraphLinkedList.cpp
+++ src/graphs/GraphLinkedList.cpp
@@ -1,1 +1,50 @@
 #include <GraphLinkedList.hpp>
+
+GraphLinkedList::GraphLinkedList(int n) : _n(n), _edges(0) {
+    this->_adjacents = new IntSLList*[n];
+    for (int i = 0; i < n; i++) {
+        this->_adjacents[i] = new IntSLList;
+    }
+}
+
+GraphLinkedList::~GraphLinkedList(void) {
+    for (int i = 0; i < this->_n; i++) {
+        delete this->_adjacents[i];
+    }
+    delete this->_adjacents;
+}
+
+int GraphLinkedList::countEdges(void) const {
+    return this->_edges;
+}
+
+bool GraphLinkedList::edgeExists(int v0, int v1) const {
+    return this->_adjacents[v0]->isInList(v1);
+}
+
+void GraphLinkedList::insertEdge(int v0, int v1) {
+    if (!this->isValidVertex(v0) || !this->isValidVertex(v1)) {
+        throw "invalid vertex";
+    }
+    if (!this->edgeExists(v0, v1)) {
+        this->_edges++;
+    }
+    this->_adjacents[v0]->addToTail(v1);
+    this->_adjacents[v1]->addToTail(v0);
+}
+
+void GraphLinkedList::deleteEdge(int v0, int v1) {
+    if (!this->isValidVertex(v0) || !this->isValidVertex(v1)) {
+        throw "invalid vertex";
+    }
+    if (!this->edgeExists(v0, v1)) {
+        return;
+    }
+    this->_edges--;
+    this->_adjacents[v0]->deleteNode(v1);
+    this->_adjacents[v1]->deleteNode(v0);
+}
+
+bool GraphLinkedList::isValidVertex(int v) const {
+    return v > -1 && v < this->_n;
+}
