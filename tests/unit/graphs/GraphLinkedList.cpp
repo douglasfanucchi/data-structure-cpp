ac332c3ff2638065268b04b2e23f0027191a2890
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-24 07:41:48 -0300

    tests: creates a linked list graph

--- tests/unit/graphs/GraphLinkedList.cpp
+++ tests/unit/graphs/GraphLinkedList.cpp
@@ -1,5 +1,14 @@
 #include <asserts.hpp>
 #include <GraphLinkedList.hpp>
 
+void test_should_create_graph()
+{
+    GraphLinkedList graph(5);
+
+    ASSERT_EQ(0, graph.countEdges());
+}
+
 void RUN_GRAPH_LINKED_LIST_TEST_SUITE()
-{}
+{
+    test_should_create_graph();
+}
