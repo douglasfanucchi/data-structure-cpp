ca1851198ef8ea245e5ab98187146a0edcdc1137
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-29 18:11:17 -0300

    tests: solves a maze with multiple edges

--- tests/unit/graphs/Maze.cpp
+++ tests/unit/graphs/Maze.cpp
@@ -29,9 +29,31 @@ static void test_should_try_to_solve_an_impossible_maze()
     ASSERT_TRUE(path.isEmpty());
 }
 
+static void test_should_find_path_of_a_maze_with_multiple_edges()
+{
+    Maze maze(5);
+    maze.insertEdge(0, 1);
+    maze.insertEdge(0, 2);
+    maze.insertEdge(0, 3);
+    maze.insertEdge(0, 4);
+    maze.insertEdge(1, 3);
+    maze.insertEdge(3, 2);
+    maze.insertEdge(2, 4);
+
+    IntSLList path = maze.solve(0, 4);
+
+    ASSERT_FALSE(path.isEmpty());
+    ASSERT_EQ(0, path.deleteFromHead());
+    ASSERT_EQ(1, path.deleteFromHead());
+    ASSERT_EQ(3, path.deleteFromHead());
+    ASSERT_EQ(2, path.deleteFromHead());
+    ASSERT_EQ(4, path.deleteFromHead());
+}
+
 void RUN_MAZE_TEST_SUITE()
 {
     test_should_create_a_maze();
     test_should_find_path_of_a_simple_maze();
     test_should_try_to_solve_an_impossible_maze();
+    test_should_find_path_of_a_maze_with_multiple_edges();
 }
