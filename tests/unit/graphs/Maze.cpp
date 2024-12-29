d9e85f53c882c3ac11572a85c914959badd6ffc6
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-12-29 16:29:49 -0300

    tests: solves simple maze path

--- tests/unit/graphs/Maze.cpp
+++ tests/unit/graphs/Maze.cpp
@@ -8,7 +8,20 @@ static void test_should_create_a_maze()
     ASSERT_EQ(0, maze.countEdges());
 }
 
+static void test_should_find_path_of_a_simple_maze()
+{
+    Maze maze(2);
+    maze.insertEdge(0, 1);
+
+    IntSLList path = maze.solve(0, 1);
+
+    ASSERT_FALSE(path.isEmpty());
+    ASSERT_EQ(0, path.deleteFromHead());
+    ASSERT_EQ(1, path.deleteFromHead());
+}
+
 void RUN_MAZE_TEST_SUITE()
 {
     test_should_create_a_maze();
+    test_should_find_path_of_a_simple_maze();
 }
